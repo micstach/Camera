6bcf1eaf5e8892830779a4210f23b318f306ed45
micstach <micstach@poczta.onet.pl>
2016-07-16 19:57:39 +0200

    update

--- camera.js
+++ camera.js
@@ -35,29 +35,16 @@ window.addEventListener("DOMContentLoaded", function() {
 		}, errBack);
 	}
 
-	function ab2str(buf) {
-	  return String.fromCharCode.apply(null, new Uint16Array(buf));
-	}
-
-	function str2ab(str) {
-	  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-	  var bufView = new Uint16Array(buf);
-	  for (var i=0, strLen=str.length; i<strLen; i++) {
-	    bufView[i] = str.charCodeAt(i);
-	  }
-	  return buf;
-	}
-
 	var captureImageLoop = function() {
 		canvas.getContext("2d").drawImage(video, 0, 0, 160, 120);
 
-		var imageData = canvas.toDataURL();
-		var noprefix = imageData.replace("data:image/png;base64,", "");
+		var imageData = canvas.toDataURL('image/jpeg');
+		var noprefix = imageData.replace("data:image/jpeg;base64,", "");
 		var unbased = atob(noprefix);
 
     connection.send(unbased);
 
-		setTimeout(captureImageLoop, 1000);
+		setTimeout(captureImageLoop, 250);
 	}
 
   var initializeConnection = function() {
@@ -80,7 +67,8 @@ window.addEventListener("DOMContentLoaded", function() {
 		} 
 
 		if (messageData.data.length > 0) {
-			$("#video-" + messageData.id).attr('src', "data:image/png;base64, " + btoa(messageData.data));
+
+			$("#video-" + messageData.id).attr('src', "data:image/jpeg;base64, " + btoa(messageData.data));
 		} else {
 			$("#video-" + messageData.id).remove();
 		}
