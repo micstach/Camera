909377223dc45a28e36d36113797e5359774eacd
micstach <micstach@poczta.onet.pl>
2016-07-17 01:57:35 +0200

    update

--- camera.js
+++ camera.js
@@ -4,7 +4,7 @@ var connection = null;
 // audio context
 var bufferSize = 4096;
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-var arrayBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+var arrayBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate/2);
 
 function convertFloat32ToInt16(buffer) {
   l = buffer.length;
