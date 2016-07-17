045b9aee796d001ce94e05d2008c45eacd698784
micstach <micstach@poczta.onet.pl>
2016-07-17 13:29:14 +0200

    update

--- camera.js
+++ camera.js
@@ -26,7 +26,7 @@ function convertInt16ToFloat32(buffer) {
   l = buffer.length;
   buf = new Float32Array(l);
   while (l--) {
-    buf[l] = parseFloat(buffer[l]) / 32000.0 ;
+    buf[l] = parseFloat(buffer[l]) / 8192.0 ;
   }
   return buf;
 }
