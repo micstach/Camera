925b11ff537cb921c5218ba5428d7a9245be6469
micstach <micstach@poczta.onet.pl>
2016-07-17 13:48:47 +0200

    update

--- camera.js
+++ camera.js
@@ -2,7 +2,7 @@
 var connection = null;
 
 // audio context
-var bufferSize = 16384;
+var bufferSize = 8192;
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 var scale = 2 ;
